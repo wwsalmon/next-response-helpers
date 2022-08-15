5209390188985780c2ff45f2e3f3a9fa25268df2
Samson Zhang <wwsamson@yahoo.com>
2022-08-15 10:51:49 -0400

    update packages and add types

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import { NextApiResponse } from "next";
+export const res403: (res: NextApiResponse, message?: string) => any;
+export const res400: (res: NextApiResponse, message?: string) => any;
+export const res500: (res: NextApiResponse, e: Error) => any;
+export const res405: (res: NextApiResponse, message?: string) => any;
+export const res404: (res: NextApiResponse, message?: string) => any;
+export const res200: (res: NextApiResponse, data?: any) => any;
+export const ssrRedirect: (destination: string) => {
+    redirect: {
+        permanent: boolean;
+        destination: string;
+    };
+};
+export const ssr404: {
+    notFound: true;
+};
+
+//# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -2,37 +2,29 @@ function $parcel$export(e, n, v, s) {
   Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
 }
 
-$parcel$export(module.exports, "res500", () => $a54b95d52ae3295a$export$c837572cfe3bb51);
-$parcel$export(module.exports, "res200", () => $a54b95d52ae3295a$export$f1b2a327fa2c22d3);
-$parcel$export(module.exports, "ssr404", () => $a54b95d52ae3295a$export$479425b9cf6d8de);
-$parcel$export(module.exports, "res405", () => $a54b95d52ae3295a$export$f0ebfaff75d37cf9);
-$parcel$export(module.exports, "res404", () => $a54b95d52ae3295a$export$4e794a523b53b9a7);
-$parcel$export(module.exports, "ssrRedirect", () => $a54b95d52ae3295a$export$83a078795c8a5ac1);
-$parcel$export(module.exports, "res403", () => $a54b95d52ae3295a$export$78f3cf4e0a7e1f43);
-$parcel$export(module.exports, "res400", () => $a54b95d52ae3295a$export$29c2e71bcfeca1ac);
-const $a54b95d52ae3295a$export$78f3cf4e0a7e1f43 = (res, message)=>res.status(403).send(message || "Unauthed")
-;
-const $a54b95d52ae3295a$export$29c2e71bcfeca1ac = (res, message)=>res.status(400).send(message || "Missing parameters")
-;
-const $a54b95d52ae3295a$export$c837572cfe3bb51 = (res, e)=>res.status(500).json({
+$parcel$export(module.exports, "res403", () => $80bd448eb6ea085b$export$e7e51890a5f9651c);
+$parcel$export(module.exports, "res400", () => $80bd448eb6ea085b$export$afbc1fdc383f9ea2);
+$parcel$export(module.exports, "res500", () => $80bd448eb6ea085b$export$6317d51344e3e0ef);
+$parcel$export(module.exports, "res405", () => $80bd448eb6ea085b$export$d9468181a51a3dcf);
+$parcel$export(module.exports, "res404", () => $80bd448eb6ea085b$export$ab869b3759209d34);
+$parcel$export(module.exports, "res200", () => $80bd448eb6ea085b$export$3913c8412cb9367);
+$parcel$export(module.exports, "ssrRedirect", () => $80bd448eb6ea085b$export$716b763dc4a24da9);
+$parcel$export(module.exports, "ssr404", () => $80bd448eb6ea085b$export$82de3e9dff96f5);
+const $80bd448eb6ea085b$export$e7e51890a5f9651c = (res, message)=>res.status(403).send(message || "Unauthed");
+const $80bd448eb6ea085b$export$afbc1fdc383f9ea2 = (res, message)=>res.status(400).send(message || "Missing parameters");
+const $80bd448eb6ea085b$export$6317d51344e3e0ef = (res, e)=>res.status(500).json({
         error: e
-    })
-;
-const $a54b95d52ae3295a$export$f0ebfaff75d37cf9 = (res, message)=>res.status(405).send(message || "Method not allowed")
-;
-const $a54b95d52ae3295a$export$4e794a523b53b9a7 = (res, message)=>res.status(404).send(message || "Resource not found")
-;
-const $a54b95d52ae3295a$export$f1b2a327fa2c22d3 = (res, data)=>res.status(200).json(data || {
-    })
-;
-const $a54b95d52ae3295a$export$83a078795c8a5ac1 = (destination)=>({
+    });
+const $80bd448eb6ea085b$export$d9468181a51a3dcf = (res, message)=>res.status(405).send(message || "Method not allowed");
+const $80bd448eb6ea085b$export$ab869b3759209d34 = (res, message)=>res.status(404).send(message || "Resource not found");
+const $80bd448eb6ea085b$export$3913c8412cb9367 = (res, data)=>res.status(200).json(data || {});
+const $80bd448eb6ea085b$export$716b763dc4a24da9 = (destination)=>({
         redirect: {
             permanent: false,
             destination: destination
         }
-    })
-;
-const $a54b95d52ae3295a$export$479425b9cf6d8de = {
+    });
+const $80bd448eb6ea085b$export$82de3e9dff96f5 = {
     notFound: true
 };
 
